9d6dbb5d224d30469f56b1ee9f381b25131f4973
shenoyshre22 <shreyashenoy0222@gmail.com>
2026-03-19 18:01:20 +0530

    added 3 new topics and comments for each

--- topics.py
+++ topics.py
@@ -4,13 +4,16 @@
 # so we only define the topics in one place that we need , pls add any more if you think would be interesting to have :)
 
 TOPICS = [
-    "sports",
-    "world_news",
-    "national_news",
-    "pesu_live",
-    "music",
-    "gaming",
-    "hollywood",
-    "bollybuzz",
-    "stocks"
+    "sports",     # added: for sports news and updates, like football, cricket, etc.
+    "world_news",     # added: for global news and updates , trump triggering whom 
+    "national_news",    # added: for news related to India
+    "pesu_live",           #our fave :)
+    "music",       # added: waiting for OR3
+    "gaming",          # added: pretty sure this would be filled w  GTA6 fake updates
+    "hollywood",         # added: did timothee win an oscar or smth?? idk but added it anyway for movie news and gossip
+    "bollybuzz",       # added: for Bollywood  gossip altho its a nayy rn
+    "stocks",           # added: for stock market updates??? idk man for interested people ig :')
+    "technology",   # added: for tech news and updates
+    "memes",        # added: for memes and humour
+    "science",     # added: for science and discoveries
 ]
